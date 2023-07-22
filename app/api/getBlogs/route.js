5184d8f6da96645c0f60ad86fe96be22a97ff5b8
shefat <shefatislam1946@gmail.com>
2023-07-22 17:02:16 +0600

    fourth commit

--- app/api/getBlogs/route.js
+++ app/api/getBlogs/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import prisma from '@/libs/prismadb'
+
+export const dynamic = 'force-dynamic'
 export async function GET(){
     const blogs=await prisma.Post.findMany({});
     return NextResponse.json(blogs);
--- app/api/postBlog/route.js
+++ app/api/postBlog/route.js
@@ -2,6 +2,8 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from '@/libs/prismadb'
 
+
+export const dynamic = 'force-dynamic'
 export async function POST(req){
     const {id}=await getCurrentUser()
     const data=await req.json();
